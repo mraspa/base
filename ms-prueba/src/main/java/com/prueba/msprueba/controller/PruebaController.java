3e7d2627187556515bf73d8e1180a394a6f1c2df
Matias Raspa <mraspa@mobydigital.com>
2024-02-08 19:58:14 -0300

    New: ms-prueba + properties en config-data

---
+++ ms-prueba/src/main/java/com/prueba/msprueba/controller/PruebaController.java
@@ -0,0 +1,17 @@
+package com.prueba.msprueba.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pruebas")
+public class PruebaController {
+
+    @GetMapping
+    ResponseEntity<String> probandoMicroservicio(){
+        return new ResponseEntity<>("Hola Mundo",HttpStatus.OK);
+    }
+}
---
+++ ms-prueba/src/test/java/com/prueba/msprueba/MsPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.msprueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
